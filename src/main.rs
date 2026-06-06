895bf19eb7e892692dfd21adea10d2e99903b1dc
莯凛 <mokurin000@gmail.com>
2026-06-06 16:52:22 +0800

    feat: generate sha512sum

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,38 @@ use std::random::{DefaultRandomSource, RandomSource};
 #[repr(align(128))]
 struct Aligned<T>(T);
 
+fn payload_per_thread(payload: fn()) {
+    std::iter::once(payload)
+        .cycle()
+        .take(num_cpus::get())
+        .map(std::thread::spawn)
+        .collect::<Vec<_>>()
+        .into_iter()
+        .for_each(|handle| _ = handle.join());
+}
+
 fn main() {
-    std::iter::once(|| {
+    let payload = || {
         let mut buf = Aligned::<_>([0u8; 128]);
         loop {
             DefaultRandomSource.fill_bytes(&mut buf.0);
         }
-    })
-    .cycle()
-    .take(num_cpus::get())
-    .map(std::thread::spawn)
-    .collect::<Vec<_>>()
-    .into_iter()
-    .for_each(|handle| _ = handle.join());
+    };
+
+    #[cfg(feature = "sha")]
+    let payload = if std::env::args().find(|arg| arg == "--sha").is_some() {
+        payload
+    } else {
+        || {
+            // 8 MiB per thread
+            let mut buf = Aligned::<_>([0u8; 8 * 1024 * 1024]);
+            DefaultRandomSource.fill_bytes(&mut buf.0);
+
+            loop {
+                std::hint::black_box(ring::digest::digest(&ring::digest::SHA512, &buf.0));
+            }
+        }
+    };
+
+    payload_per_thread(payload);
 }
